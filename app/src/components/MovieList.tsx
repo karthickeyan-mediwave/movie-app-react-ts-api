c89bb332215bce0ec64c90e1e6ac5cec6f54a6ab
karthickeyan-mediwave <karthickeyan@mindwaveventures.com>
2023-10-27 11:34:31 +0530

    movielist-rearrange

--- app/src/components/MovieList.tsx
+++ app/src/components/MovieList.tsx
@@ -1,56 +1,49 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import { Link } from "react-router-dom";
-// import { useParams } from "react-router-dom";
-import { getMovies } from "./Learning";
 import { useNavigate } from "react-router-dom";
 import { Todo } from "../types";
 import Layout from "./Layout";
+import { deleteMovie, getMovies } from "./apiService";
+import Modal from "./Modal";
 
-const TodoList: React.FC = () => {
-  // const { id } = useParams<{ id: string }>();
+const MovieList: React.FC = () => {
   const navigate = useNavigate();
-
   const [todos, setTodos] = useState<Todo[]>([]);
-  const [dialog, setdialog] = useState(false);
+  const [dialog, setDialog] = useState(false);
 
   useEffect(() => {
     async function getMoviesFromAPI() {
       try {
         const response = await getMovies();
-        setTodos(response.data);
+        setTodos(response);
       } catch (error) {
         console.log(error);
-      } finally {
       }
     }
     getMoviesFromAPI();
   }, []);
 
-  const handledelete = (id: number) => {
-    axios
-      .delete<Todo[]>(`http://localhost:5476/movies/${id}`)
-      .then((response) => {
-        console.log("Todo deleted:", response.data);
-        console.log(`${id}`);
-        setdialog(true);
-      })
-      .catch((error) => {
-        console.error("Error updating :", error);
-      });
+  const handleDelete = async (id: number) => {
+    try {
+      await deleteMovie(id);
+      console.log("Todo deleted:", id);
+      setDialog(true);
+    } catch (error) {
+      console.error("Error deleting todo:", error);
+    }
   };
 
   return (
     <Layout title="movies">
       <h1>Movies</h1>
       <button onClick={() => navigate("/add")} className="home-add-btn">
-        <i className="fa fa-plus  kkk"> </i>
+        <i className="fa fa-plus kkk"> </i>
       </button>
 
       <div className="grid">
         {todos.map((todo) => (
           <div key={todo.id}>
-            <article key={todo.id}>
+            <article>
               <h2>movie- {todo.title}</h2>
               <h3> Year: {todo.year}</h3>
 
@@ -62,7 +55,7 @@ const TodoList: React.FC = () => {
                 </button>
                 <button
                   className="delete-btn"
-                  onClick={() => handledelete(todo.id)}
+                  onClick={() => handleDelete(todo.id)}
                 >
                   <i className="fa fa-trash-o"></i>
                 </button>
@@ -72,16 +65,9 @@ const TodoList: React.FC = () => {
         ))}
       </div>
 
-      <dialog open={dialog}>
-        <article>
-          <p>Api request sucess</p>
-          <footer>
-            <button onClick={() => navigate("/")}>ok</button>
-          </footer>
-        </article>
-      </dialog>
+      <Modal dialog={dialog} setDialog={setDialog} />
     </Layout>
   );
 };
 
-export default TodoList;
+export default MovieList;
